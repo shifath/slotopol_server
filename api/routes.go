26d539a48e3ce5f24e422719a3fd7a2acf9f6c97
shifath.shabbib <74237711+shifath@users.noreply.github.com>
2025-12-01 00:50:26 +0600

    Update routes.go

--- api/routes.go
+++ api/routes.go
@@ -11,7 +11,6 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"xorm.io/xorm"
 
-	"github.com/gin-contrib/cors"
 	cfg "github.com/slotopol/server/config"
 )
 
@@ -130,14 +129,6 @@ func SetupRouter(r *gin.Engine) {
 	r.NoRoute(Handle404)
 	r.NoMethod(Handle405)
 	//r.Use(gzip.Gzip(gzip.DefaultCompression))
-	r.Use(cors.New(cors.Config{
-            AllowOrigins:     []string{"https://shifath.github.io"}, // Specify the exact origin of your index.html
-            AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-            AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
-            ExposeHeaders:    []string{"Content-Length"},
-            AllowCredentials: true,
-            // MaxAge:           12 * time.Hour, // Optional: cache preflight requests for 12 hours
-        }))
 
 	r.Any("/ping", ApiPing)
 	r.GET("/servinfo", ApiServInfo)
